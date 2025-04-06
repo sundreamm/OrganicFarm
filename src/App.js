6385bc5c960c777b317e11ebc04ecd692b34ac2c
Daisyii <yuezixi1@gmail.com>
2025-04-06 16:35:40 -0230

    no message

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Welcome to Organic Farm!
+        </h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
